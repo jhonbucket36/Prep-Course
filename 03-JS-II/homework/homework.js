9d4a52918b8253afb7a01d0c1e56ee2298f277f3
Jhonbucket36 <juan.castro81@inacapmail.cl>
2023-02-02 11:56:10 -0300

    avances

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -89,6 +89,8 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  return numero < 50 && numero > 20 ? true : false
 }
 
 function esEntero(numero) {
