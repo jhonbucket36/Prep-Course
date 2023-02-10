69ed98211926c0a08dfd9bb4e2dd92e41bb59ad8
Jhonbucket36 <juan.castro81@inacapmail.cl>
2023-02-10 00:25:11 -0300

    cambios/no terminado

--- 03-JS-II/homework/tempCodeRunnerFile.js
+++ 03-JS-II/homework/tempCodeRunnerFile.js
@@ -1,23 +1,23 @@
-function operadoresLogicos(num1, num2, num3) {
-  //La función recibe tres números distintos. 
-  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
-  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
-  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
-  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  
-   if (num1 > num2 && num1 > num3 && num1 > 0) {
-    return "Número 1 es mayor y positivo";
-  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
-    return "Hay negativos";
-  } else if (num3 > num1 && num3 > num2) {
-    return ++ num3 ;
-  } else if (num1 === 0 || num2 === 0 || num3 === 0) {
-    return "Error";
-  } else {
-    return false;
-  }
 
+function tieneTresDigitos(numero){
+  //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
+  //Escribe tu código aquí
+  
+  function tieneTresDigitos(numero){
+  
+    let num1 = numero.toString().length
+    
+    if(num1 === 3){
+      return true
+      
+    }else{
+      return false
+    }
+    
+    
+  }
+  
+  
 }
 
-console.log(operadoresLogicos(1, 2, 3));
+console.log(tieneTresDigitos(100))
