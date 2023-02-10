ed1411e6513f30ee66bd532be9e665103551147c
Jhonbucket36 <juan.castro81@inacapmail.cl>
2023-02-10 12:45:51 -0300

    cambios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -104,10 +104,11 @@ function numeroMasGrande(numeros) {
   //return Math.max(...numeros);
 }
 
-function multiplicarArgumentos() {
+function multiplicarArgumentos(arguments) {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  return arguments.reduce((acc,val) => acc * val)
 }
 
 function cuentoElementos(arreglo) {
