8363119422e4bda79a94cc77c0e1cd91ee1c0b08
Jhonbucket36 <juan.castro81@inacapmail.cl>
2023-02-12 15:59:56 -0300

    ultimos cambios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -243,6 +243,7 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  fdkk
 }
 
 // No modificar nada debajo de esta línea
